589aae12358ec65fef597e92c1df0c01a74b05f5
Raghav Sharma <raghavsharma@Raghavs-MacBook-Pro.local>
2023-09-15 16:07:22 +0530

    FieldAuditApp with QR thing and BottomSlider done with Example POST Api done and 2nd API done also, 3rd also done just detail audit page done and some extra UI Changes and also decryption and shared preference done also. Logout process happeningg too. Correct Navigation in Done also added, with Audit api, with Navigation Drawer (Before Error Handling)

--- app/src/main/java/com/chalo/fieldauditapp/ErrorHandling/Error.kt
+++ app/src/main/java/com/chalo/fieldauditapp/ErrorHandling/Error.kt
@@ -1,7 +1,7 @@
 package com.chalo.fieldauditapp.ErrorHandling
 
 import retrofit2.Call
-fun <T:Any, Req:Any> ApiCall(ReqBody:Req, headerFlag:Boolean, headerBody:Pair<String,String>, queryFlag:Boolean, queryBody:Pair<String,String>, call: Call<T>, respType:T):Pair<Boolean,T>
+fun <T:Any, Req:Any> ApiCall(ReqBody:Req, headerFlag:Boolean, headerBody:Pair<String,String>, queryFlag:Boolean, queryBody:Pair<String,String>, call: Call<T>, respType: Class<T>):Pair<Boolean,T>
 {
 
     val resp:Pair<Boolean,T> =Pair(false,null)
