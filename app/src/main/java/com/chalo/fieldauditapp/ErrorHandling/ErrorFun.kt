98b6c6bf1cd03806398eb5e18c5598faaee5bfb2
Raghav Sharma <raghavsharma@Raghavs-MacBook-Pro.local>
2023-09-20 16:17:23 +0530

    FieldAuditApp with QR thing and BottomSlider done with Example POST Api done and 2nd API done also, 3rd also done just detail audit page done and some extra UI Changes and also decryption and shared preference done also. Logout process happeningg too. Correct Navigation in Done also added, with Audit api, with Navigation Drawer (Before Error Handling)

--- app/src/main/java/com/chalo/fieldauditapp/ErrorHandling/ErrorFun.kt
+++ app/src/main/java/com/chalo/fieldauditapp/ErrorHandling/ErrorFun.kt
@@ -23,29 +23,10 @@ import retrofit2.Response
 @RequiresApi(Build.VERSION_CODES.R)
 suspend fun <S> ApiCall(call: Call<S>, activity: MainActivity): Pair<Boolean, Response<S>?> {
 
-
     val loading= Loading_Dialog(activity)
     loading.start()
-//    ResourcesLoade
-//    val url: URL = Launcher::class.java.getResource(name)
-//    val gh:$respType
-//    CreateAuditAPI as T
-//    somethingThatReturnsInt() as T
-    Log.d("Check1", "Before navHost")
-    //val navHostFragment = MainActivity().supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-    Log.d("Check1", "After navHost")
-    //var navController: NavController
-    //navController = navHostFragment.findNavController()
-    Log.d("Check1", "After navController")
+
     val navController = Navigation.findNavController(activity, R.id.nav_host_fragment)
-
-    //val nvc=findNavController(R.id.nav_host_fragment)
-
-
-    //val graphInflater = navHostFragment.navController.navInflater
-//    val navGraph = graphInflater.inflate(R.navigation.nav_graph)
-//    navController = navHostFragment.navController
-
 
     var flag: Boolean = false
     var res: Response<S>? = null
@@ -53,96 +34,47 @@ suspend fun <S> ApiCall(call: Call<S>, activity: MainActivity): Pair<Boolean, Re
     var cd:Int=200
     var flag2:Boolean=false
 
-    //navController = MainActivity().navHostFragment.findNavController()
-    //val resp:Pair<Boolean,Response<S>?>
-
-    //val job=withContext(Dispatchers.Main){
     val job = CoroutineScope(Dispatchers.IO).async {
-        Log.d("Check1", "Inside coroutine")
         var check: Boolean = false
 
-        //check=false
         call.enqueue(object : Callback<S> {
             override fun onResponse(call: Call<S>, response: Response<S>) {
-                Log.d("Check1", "onResponse")
                 fail=false
                 check = true
                 loading.isDismiss()
 
                 res = response
-                Log.d("SuccessapiLog", response.code().toString())
-                Log.d("SuccessapiLog", response.message().toString())
-                Log.d("SuccessapiLog", response.body().toString())
-                if (response.code() >= 500) {
-                    //Toast.makeText(context,"ServerError",Toast.LENGTH_LONG).show()
-                    //*NavController().navigate(R.id.action_loginFragment_to_errorDetailsFragment)
-                    //MainActivity().findNavController().navigate(R.id.action_loginFragment_to_errorDetailsFragment)
-                    //navController.navigate(R.id.errorDetailsFragment)
-                    cd=500
-                    //activity.findNavController(R.id.errorDetailsFragment)
-                    //return Pair(false, null)
-                    //(Activity as MainActivity).
-                } else if (response.code() >= 400) {
-                    //Toast.makeText(activity , "Wrong details", Toast.LENGTH_LONG).show()
-                    cd=400
-                    //findNavController().navigate(R.id.action_loginFragment_to_errorDetailsFragment) //Remove
-                    //*binding.editTextUserlayout.error="This User ID does not exist"
-                } else if (response.code() >= 200)     //CORRECT THIS ***+++**********************
-                    {
-                        //*val token= response.body()?.get("token")
-                        Log.d("Check1", "Inside 200 cond")
+
+                if (response.code() >= 500) { cd=500 }
+                else if (response.code() >= 400) { cd=400 }
+                else if (response.code() >= 200)
+                {
                         val key = "token"
                         flag = true
-
-//                        res = response
-                        //MainActivity().findNavController(R.id.errorDetailsFragment)
-                        //*saveData(key,token.toString())
-                        //*binding.editTextUserlayout.error=null
-                        //*findNavController().navigate(R.id.action_loginFragment_to_busSelectionFragment)
-                    } else {
+                }
+                else
+                {
                         Toast.makeText(
                             (MainActivity()),
                             "Enter correct Login Details",
                             Toast.LENGTH_SHORT
                         ).show()
-                    }
-                //binding.code2TV.text=response.code().toString()
-                //Toast.makeText(context, response.code(), Toast.LENGTH_LONG)
+                }
+
             }
 
             override fun onFailure(call: Call<S>, t: Throwable) {
                 check = true
                 fail=true
-                Log.d("Check1", "onFailure")
-                Log.d("ErrorapiLog", t.toString())
-
                 cd=1000
-
-                //activity.findNavController(R.id.noNetworkFragment)
-                //navController.navigate(R.id.noNetworkFragment)
-
                 loading.isDismiss()
-                //Toast.makeText(context,"NO INTERNET CONNECTION TO LOGIN 22",Toast.LENGTH_LONG).show()
-                //*findNavController().navigate(R.id.action_loginFragment_to_noNetworkFragment)
-                //binding.code2TV.text=t.message.toString()
             }
         })
-        Log.d("Check1", "At the end of Coroutine + check=${check}")
+
         while (check == false) {
-            Log.d("Check1", "in check + check=${check}")
+//            Log.d("Check1", "in check + check=${check}")
         }
-//        if(fail==true)
-//        {
-//            withContext(Dispatchers.Main){
-//                //navController.navigate(R.id.noNetworkFragment)
-//                //NavHostFragment.findNavController(R.navigation)
-//                Log.d("Check1","In fail==true")
-////                activity.findNavController(R.id.noNetworkFragment)
-////                activity.
-//                navController.navigate(R.id.noNetworkFragment)
-//            }
-//        }
-        Log.d("Check1","*******")
+
         if(flag==false)
         {
             withContext(Dispatchers.Main)
@@ -171,17 +103,6 @@ suspend fun <S> ApiCall(call: Call<S>, activity: MainActivity): Pair<Boolean, Re
         check=false
     }
     job.await()
-    Log.d("Check1", "After Enqueue and flag=${flag2}")
-
 
     return Pair(fail, res)
-}
-
-fun <T:Any> A(cx:Int,valw:T):Int
-{
-    return 2*cx
-}
-fun sj(cx:Int):Int
-{
-    return 2*cx
 }
